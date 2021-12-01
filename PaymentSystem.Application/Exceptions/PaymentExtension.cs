65a83e05b62b72e1b67911bda90e5b9b7bc0e24e
VitaliyPasko <48154293+VitaliyPasko@users.noreply.github.com>
2021-12-02 01:43:54 +0600

    Добавил логику обработки платежа.
    #3

---
+++ PaymentSystem.Application/Exceptions/PaymentExtension.cs
@@ -0,0 +1,21 @@
+﻿using Common.Enums;
+using PaymentSystem.ApplicationLayer.Services.PaymentService.Dto;
+using PaymentSystem.ApplicationLayer.Services.PaymentService.Models;
+
+namespace PaymentSystem.ApplicationLayer.Exceptions
+{
+    public static class PaymentExtension
+    {
+        public static Payment MapToPayment(this PaymentDto paymentDto, ProviderType providerType)
+        {
+            return new Payment
+            {
+                Amount = paymentDto.Amount,
+                Phone = paymentDto.Phone,
+                ExternalNumber = paymentDto.ExternalNumber,
+                ProviderType = providerType,
+                Status = paymentDto.Status
+            };
+        }
+    }
+}

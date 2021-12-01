4d4494f101733a23a8f2f8bd7e4b7e31ab56be82
VitaliyPasko <48154293+VitaliyPasko@users.noreply.github.com>
2021-12-02 01:51:29 +0600

    Добавил котроллер платежей.
    #3

---
+++ PaymentSystem.Infrastructure/Controllers/PaymentController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using Common.ResponseDtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using PaymentSystem.ApplicationLayer.Services.PaymentService.Dto;
+using PaymentSystem.ApplicationLayer.Services.PaymentService.Interfaces;
+
+namespace PaymentSystem.Infrastructure.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PaymentController : ControllerBase
+    {
+        private readonly IPaymentService _paymentService;
+
+        public PaymentController(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(PaymentDto payment)
+        {
+            try
+            {
+                StringValues requestId = Request.Headers["RequestId"].ToString();
+                var result = await _paymentService.CreatePayment(payment, requestId);
+            
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return Ok(new Response
+                {
+                    Message = "Неизвестная ошибка.",
+                    StatusCode = Common.Enums.StatusCode.UnableError
+                });
+            }
+        }
+    }
+}

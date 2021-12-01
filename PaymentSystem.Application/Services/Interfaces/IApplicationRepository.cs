1cb33c36b35273a2e608c2826dde36dc0447413b
VitaliyPasko <48154293+VitaliyPasko@users.noreply.github.com>
2021-12-01 23:03:51 +0600

    Добавил репозиторий для доступа к платежам в базе данных.
    #1

---
+++ PaymentSystem.Application/Services/Interfaces/IApplicationRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace PaymentSystem.ApplicationLayer.Services.Interfaces
+{
+    public interface IApplicationRepository<T> where T : class
+    {
+        Task Add(T entity);
+    }
+}
---
+++ PaymentSystem.Infrastructure/Repository/PaymentRepository/PaymentRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using PaymentSystem.ApplicationLayer.Services.Interfaces;
+using PaymentSystem.Infrastructure.Data;
+using PaymentSystem.Infrastructure.Models;
+
+namespace PaymentSystem.Infrastructure.Repository.PaymentRepository
+{
+    public class PaymentRepository : IApplicationRepository<Payment>
+    {
+        private readonly ApplicationDbContext _db;
+
+        public PaymentRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task Add(Payment entity)
+        {
+            await _db.AddAsync(entity);
+            await _db.SaveChangesAsync();
+        }
+    }
+}

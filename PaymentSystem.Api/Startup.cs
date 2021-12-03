ee73352b8259947e2cf392f27796624450a4704f
VitaliyPasko <48154293+VitaliyPasko@users.noreply.github.com>
2021-12-03 21:23:22 +0600

    Добавить логгирование.
    ***
    Подключил Serilog. Обернул ллгами приложение.
    ***
    
    #6

--- PaymentSystem.Api/Startup.cs
+++ PaymentSystem.Api/Startup.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -12,15 +11,20 @@ namespace PaymentSystemApi
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public IConfiguration Configuration { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllersWithViews();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo {Title = "PaymentSystemApi", Version = "v1"});
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "PaymentSystem.Api", Version = "v1"});
             });
             services.AddInfrastructureServices(Configuration);
         }
@@ -30,14 +34,14 @@ namespace PaymentSystemApi
         {
             if (env.IsDevelopment())
             {
-                app.UseSwagger();
                 app.UseDeveloperExceptionPage();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PaymentSystemApi v1"));
             }
+            
+            app.UseSerilogRequestLogging();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseSerilogRequestLogging();
             app.UseRouting();
+            app.UseSwagger();
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
--- PaymentSystem.Infrastructure/Controllers/PaymentController.cs
+++ PaymentSystem.Infrastructure/Controllers/PaymentController.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Common.ResponseDtos;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Primitives;
 using PaymentSystem.ApplicationLayer.Services.PaymentService.Dto;
 using PaymentSystem.ApplicationLayer.Services.PaymentService.Interfaces;
@@ -9,14 +10,20 @@ using PaymentSystem.ApplicationLayer.Services.PaymentService.Interfaces;
 namespace PaymentSystem.Infrastructure.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/payments")]
     public class PaymentController : ControllerBase
     {
         private readonly IPaymentService _paymentService;
+        private readonly ILogger<PaymentController> _logger;
+        private readonly Type _type;
 
-        public PaymentController(IPaymentService paymentService)
+        public PaymentController(
+            IPaymentService paymentService, 
+            ILogger<PaymentController> logger)
         {
             _paymentService = paymentService;
+            _logger = logger;
+            _type = GetType();
         }
 
         [HttpPost]
@@ -25,12 +32,18 @@ namespace PaymentSystem.Infrastructure.Controllers
             try
             {
                 StringValues requestId = Request.Headers["RequestId"].ToString();
+                _logger.LogInformation("{@Controller}.Create Запрос {@Payment}. RequestId: {@RequestId}", 
+                    _type, payment, requestId);
+                
                 var result = await _paymentService.CreatePayment(payment, requestId);
             
                 return Ok(result);
             }
             catch (Exception e)
             {
+                _logger.LogTrace(e, e.Message);
+                _logger.LogError("{@Controller}.Create Ошибка: {@Message}. Данные: {@Data}", _type, e.Message, payment);
+                
                 return Ok(new Response
                 {
                     Message = "Неизвестная ошибка.",
